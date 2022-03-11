e9783a06d9fb83925923b4f965d2db155819715c
Mary Priscilla A <mary.priscilla@thoughtworks.com>
2022-03-11 15:43:03 +0530

    Adding characterstic test for Customer

--- src/test/java/com/thoughtworks/movierental/CustomerTests.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTests.java
@@ -2,9 +2,23 @@ package com.thoughtworks.movierental;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CustomerTest {
 
     @Test
     public void test() {
+        Customer customer = new Customer("Dean");
+        customer.addRental(new Rental(new Movie("The Call", Movie.NEW_RELEASE), 10));
+        customer.addRental(new Rental(new Movie("Speed", Movie.REGULAR), 3));
+        customer.addRental(new Rental(new Movie("Doll", Movie.CHILDRENS), 5));
+
+        assertEquals("Rental Record for Dean\n" +
+                "\tThe Call\t30.0\n" +
+                "\tSpeed\t3.5\n" +
+                "\tDoll\t4.5\n" +
+                "Amount owed is 38.0\n" +
+                "You earned 4 frequent renter points", customer.statement());
     }
+
 }
